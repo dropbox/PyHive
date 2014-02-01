2544bbf650f23d9100fc938d03a7bad98c9bb3e7
Jing Wang <jing@dropbox.com>
2014-02-01 00:48:28 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name="PyHive",
+    version='0.0.1',
+    description="Python interface to Hive",
+    long_description=long_description,
+    author="Jing Wang",
+    author_email="jing@dropbox.com",
+    packages=['pyhive'],
+    classifiers=[
+        "Topic :: Database",
+    ],
+    extras_require={
+        "Presto": ['requests'],
+        "Hive": ['sasl>=0.1.3', 'thrift>=0.9.1'],
+        "SQLAlchemy": ['sqlalchemy==0.5.8'],
+    },
+    test_suite='nose.collector',
+    tests_require=[
+        'mock',
+        'nose',
+        'requests',
+        'sasl>=0.1.3',
+        'sqlalchemy==0.5.8',
+        'thrift>=0.9.1',
+    ]
+)
