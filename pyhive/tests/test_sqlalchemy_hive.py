f8d175e0e543c22417c623b9c1128b7c8a24fc72
Raghav Sharma <raghavs@dropbox.com>
2016-06-23 17:33:58 -0700

    Fix hive reflect tests

--- pyhive/tests/test_sqlalchemy_hive.py
+++ pyhive/tests/test_sqlalchemy_hive.py
@@ -74,14 +74,20 @@ class TestSqlAlchemyHive(unittest.TestCase, SqlAlchemyTestCase):
         self.assertTrue(isinstance(one_row_complex.c.float.type, types.Float))
         self.assertTrue(isinstance(one_row_complex.c.double.type, types.Float))
         self.assertTrue(isinstance(one_row_complex.c.string.type, types.String))
-        self.assertTrue(isinstance(one_row_complex.c.timestamp.type, types.TIMESTAMP) or \
+        self.assertTrue(isinstance(one_row_complex.c.timestamp.type, types.TIMESTAMP) or
                         isinstance(one_row_complex.c.timestamp.type, HiveTimestamp))
-        self.assertTrue(isinstance(one_row_complex.c.binary.type, types.NullType))
-        self.assertTrue(isinstance(one_row_complex.c.array.type, types.NullType))
-        self.assertTrue(isinstance(one_row_complex.c.map.type, types.NullType))
-        self.assertTrue(isinstance(one_row_complex.c.struct.type, types.NullType))
-        self.assertTrue(isinstance(one_row_complex.c.union.type, types.NullType))
-        self.assertTrue(isinstance(one_row_complex.c.decimal.type, types.NullType))
+        self.assertTrue(isinstance(one_row_complex.c.binary.type, types.NullType) or
+                        isinstance(one_row_complex.c.binary.type, types.String))
+        self.assertTrue(isinstance(one_row_complex.c.array.type, types.NullType) or
+                        isinstance(one_row_complex.c.binary.type, types.String))
+        self.assertTrue(isinstance(one_row_complex.c.map.type, types.NullType) or
+                        isinstance(one_row_complex.c.binary.type, types.String))
+        self.assertTrue(isinstance(one_row_complex.c.struct.type, types.NullType) or
+                        isinstance(one_row_complex.c.binary.type, types.String))
+        self.assertTrue(isinstance(one_row_complex.c.union.type, types.NullType) or
+                        isinstance(one_row_complex.c.binary.type, types.String))
+        self.assertTrue(isinstance(one_row_complex.c.decimal.type, types.NullType) or
+                        isinstance(one_row_complex.c.binary.type, HiveDecimal))
 
     @with_engine_connection
     def test_type_map(self, engine, connection):
