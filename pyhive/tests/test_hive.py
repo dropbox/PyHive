2a82c799c3983048cbb8bd5ea13a9e0858b10c3f
Junxian Wu <rupert@yahoo-inc.com>
2016-07-22 15:45:24 -0700

    test test case

--- pyhive/tests/test_hive.py
+++ pyhive/tests/test_hive.py
@@ -147,7 +147,7 @@ class TestHiveAuth(unittest.TestCase):
 
     def test_ldap_connection(self):
         import subprocess
-        subprocess.call(['../scripts/set_hive_auth_ldap.sh'])
+        subprocess.call(['/home/travis/build/axeisghost/PyHive/scripts/set_hive_auth_ldap.sh'])
         connection = hive.connect(host=_HOST, username='admin', auth='LDAP',
                                   configuration={'mapred.job.tracker': 'local'},
                                   password='test')
@@ -157,4 +157,4 @@ class TestHiveAuth(unittest.TestCase):
         self.assertEqual(cursor.fetchone(), (1,))
         self.assertEqual(cursor.rownumber, 1)
         self.assertIsNone(cursor.fetchone())
-        subprocess.call(['../scripts/revert_hive_auth.sh'])
+        subprocess.call(['/home/travis/build/axeisghost/PyHive/scripts/revert_hive_auth.sh'])
