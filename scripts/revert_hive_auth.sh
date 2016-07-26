9c1232dbe434a85e01f9978a1bef82f3a962ea15
Junxian Wu <rupert@yahoo-inc.com>
2016-07-26 11:23:43 -0700

    clean up code

--- scripts/revert_hive_auth.sh
+++
@@ -1,3 +0,0 @@
-sudo cp $(dirname $0)/travis-conf/hive/hive-site.xml /etc/hive/conf/hive-site.xml
-
-sudo service hive-server2 restart
--- scripts/set_hive_auth_ldap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -eux
-
-sudo cp $(dirname $0)/travis-conf/hive/hive-site-ldap.xml /etc/hive/conf/hive-site.xml
-
-sudo service hive-server2 restart
