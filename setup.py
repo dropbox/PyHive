beb95cde4389d16ee7c3db8c1d02c5eeed23957f
Jing Wang <jingwang13@gmail.com>
2017-07-07 22:08:18 -0700

    Work around pip casing issues in `pip install pyhive[...]`
    Fixes #129

--- setup.py
+++ setup.py
@@ -41,9 +41,9 @@ setup(
         'future',
     ],
     extras_require={
-        "Presto": ['requests>=1.0.0'],
-        "Hive": ['sasl>=0.2.1', 'thrift>=0.10.0', 'thrift_sasl>=0.1.0'],
-        "SQLAlchemy": ['sqlalchemy>=0.5.0'],
+        'presto': ['requests>=1.0.0'],
+        'hive': ['sasl>=0.2.1', 'thrift>=0.10.0', 'thrift_sasl>=0.1.0'],
+        'sqlalchemy': ['sqlalchemy>=0.5.0'],
     },
     tests_require=[
         'mock>=1.0.0',
