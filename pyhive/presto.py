19fd140938b968f74b690c3e37b7206c78eab1a1
Matthew Byng-Maddick <mb-m@users.noreply.github.com>
2020-02-28 00:24:38 +0100

    Add the last query id from presto when doing presto query onto cursor (#313)
    * Add the last query id from presto when doing presto query onto cursor
    
    * Add some assertions to the integration tests for last_query_id
    
    * Fix the testcase so it's actually doing the correct test

--- pyhive/presto.py
+++ pyhive/presto.py
@@ -145,6 +145,7 @@ class Cursor(common.DBAPICursor):
         self._poll_interval = poll_interval
         self._source = source
         self._session_props = session_props if session_props is not None else {}
+        self.last_query_id = None
 
         if protocol not in ('http', 'https'):
             raise ValueError("Protocol must be http/https, was {!r}".format(protocol))
@@ -319,6 +320,8 @@ class Cursor(common.DBAPICursor):
         assert self._state == self._STATE_RUNNING, "Should be running if processing response"
         self._nextUri = response_json.get('nextUri')
         self._columns = response_json.get('columns')
+        if 'id' in response_json:
+            self.last_query_id = response_json['id']
         if 'X-Presto-Clear-Session' in response.headers:
             propname = response.headers['X-Presto-Clear-Session']
             self._session_props.pop(propname, None)
