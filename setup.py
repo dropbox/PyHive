1f0df3eaa3941799699ab36b8b6eb78c7db8d3f2
ofekby <39569540+ofekby@users.noreply.github.com>
2020-08-18 23:19:55 +0300

    Move requirement to setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'python-dateutil',
     ],
     extras_require={
-        'presto': ['requests>=1.0.0'],
+        'presto': ['requests>=1.0.0', 'presto-types-parser>=0.0.2'],
         'hive': ['sasl>=0.2.1', 'thrift>=0.10.0', 'thrift_sasl>=0.1.0'],
         'sqlalchemy': ['sqlalchemy>=1.3.0'],
         'kerberos': ['requests_kerberos>=0.12.0'],
